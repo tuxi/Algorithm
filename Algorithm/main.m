9c343fb37eef5ed6ebfb5a5f769817ab77499687
xiaoyuan <seyooe@gmail.com>
2020-08-31 10:01:53 +0800

    Initial Commit

---
+++ Algorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Algorithm
+//
+//  Created by xiaoyuan on 2020/8/31.
+//  Copyright © 2020 xiaoyuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
